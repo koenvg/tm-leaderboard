7bd5c7f6ff241617469ecd7cccd74e237ecf9a96
koenvg <koen.van.geert@gmail.com>
2016-09-05 22:04:04 +0200

    changed route of logo

--- client/components/navigation/Navigation.jsx
+++ client/components/navigation/Navigation.jsx
@@ -7,7 +7,7 @@ export class Navigation extends Component{
         return (
             <nav className="red darken-3" role="navigation">
                 <div className="nav-wrapper container">
-                    <a id="logo-container" href="#" className="brand-logo center">666 - GAMERS</a>
+                    <a id="logo-container" href="/" className="brand-logo center">666 - GAMERS</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                         <li><a href="/add-player">Add Player</a></li>
                     </ul>
