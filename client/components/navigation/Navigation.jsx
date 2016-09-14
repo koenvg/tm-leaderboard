2822d19ff3dcbaf6710f5cb6e33f9cb928ade30c
koenvg <koen.van.geert@gmail.com>
2016-09-14 08:24:50 +0200

    small changes to css

--- client/components/navigation/Navigation.jsx
+++ client/components/navigation/Navigation.jsx
@@ -5,7 +5,7 @@ export class Navigation extends Component{
 
     render(){
         return (
-            <nav className="red darken-3" role="navigation">
+            <nav className="red " role="navigation">
                 <div className="nav-wrapper container">
                     <a id="logo-container" href="/" className="brand-logo center">666 - GAMERS</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
