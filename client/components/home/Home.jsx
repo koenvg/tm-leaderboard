a6b9bdd8dbbd9f614634ed3c2ed9bdf0eaa01a5b
koenvg <koen.van.geert@gmail.com>
2016-10-17 21:25:51 +0200

    Added banner positioning

--- client/components/home/Home.jsx
+++ client/components/home/Home.jsx
@@ -1,15 +1,15 @@
-import React, { Component, PropTypes } from 'react';
-import {LeaderBoardContainer} from '../leaderboard/Leaderboard';
-import {LatestEntriesContainer} from '../latestEntries/LatestEntries';
+import React, { Component } from 'react'
+import { LeaderBoardContainer } from '../leaderboard/Leaderboard'
+import { LatestEntriesContainer } from '../latestEntries/LatestEntries'
 
-export class Home extends Component{
-    render(){
-        return (
-            <div>
-                <div></div>
-                <LeaderBoardContainer/>
-                <LatestEntriesContainer/>
-            </div>
-        )
-    }
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div id="banner"></div>
+        <LeaderBoardContainer/>
+        <LatestEntriesContainer/>
+      </div>
+    )
+  }
 }
